81b3688ebd3015cd128f0ed59ca9aceba8855001
Wioletta Wajda <wioletta.wajda@devsoft.pl>
2021-12-08 22:35:43 +0100

    update

--- movies_web/views.py
+++ movies_web/views.py
@@ -32,21 +32,8 @@ def create_movie(request):
 @login_required
 def details_movie(request, id):
     movie = get_object_or_404(Movie, pk=id)
-    return render(request, 'details_movie.html', {'movie': movie})
-
-
-@login_required
-def update_movie(request, id):
-    movie = get_object_or_404(Movie, pk=id)
     ratings = Rating.objects.filter(film=movie)
 
-    try:
-        additional = AdditionalInfo.objects.get(movie=movie.id)
-    except AdditionalInfo.DoesNotExist:
-        additional = None
-
-    form_movie = MovieForm(request.POST or None, request.FILES or None, instance=movie)
-    form_additional = AdditionalInfoForm(request.POST or None, instance=additional)
     form_rating = RatingForm(request.POST or None)
 
     if request.method == 'POST':
@@ -55,15 +42,28 @@ def update_movie(request, id):
             rating.film = movie
             rating.save()
 
+    return render(request, 'details_movie.html', {'movie': movie, 'ratings': ratings, 'form_rating': form_rating})
+
+
+@login_required
+def update_movie(request, id):
+    movie = get_object_or_404(Movie, pk=id)
+
+    try:
+        additional = AdditionalInfo.objects.get(movie=movie.id)
+    except AdditionalInfo.DoesNotExist:
+        additional = None
+
+    form_movie = MovieForm(request.POST or None, request.FILES or None, instance=movie)
+    form_additional = AdditionalInfoForm(request.POST or None, instance=additional)
+
     if form_movie.is_valid() and form_additional.is_valid():
         movie = form_movie.save(commit=False)
         additional = form_additional.save()
         movie.dodatkowe = additional
         movie.save()
 
-    return render(request, 'form_movie.html', {'form': form_movie, 'form_additional': form_additional,
-                                               'ratings': ratings, 'form_rating': form_rating,
-                                               'new': False})
+    return render(request, 'form_movie.html', {'form': form_movie, 'form_additional': form_additional, 'new': False})
 
 
 @login_required
